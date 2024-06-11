a69b71676aaee33885aa10c5f6ba45193a956400
WhoAmI <fabian.gregory98@hotmail.de>
2024-06-11 10:29:46 +0200

    Feinheiten gemacht

--- DrawingLetters/DrawPoint.cs
+++ DrawingLetters/DrawPoint.cs
@@ -11,9 +11,6 @@
 
         public float X { get; set; }
         public float Y { get; set; }
-        public int Col { get; set; }
-        public int Row { get; set; }
-
         public int Distance { get; set; }
 
         public override string ToString() => $"(Distance:{Distance})\n";
